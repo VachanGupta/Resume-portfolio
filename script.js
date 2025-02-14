7521bf4e53b1961dc76cab57b44401ef5fc3af01
VachanGupta <myselfvachangupta@gmail.com>
2025-02-14 19:58:43 +0530

    content for skill, experience and education

---
+++ script.js
@@ -0,0 +1,16 @@
+var tablinks = document.getElementsByClassName("tab-links")
+
+var tabcontents = document.getElementsByClassName("tab-contents")
+
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tablinks){
+        tabcontent.classList.remove("active-tab")
+    }
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+}
